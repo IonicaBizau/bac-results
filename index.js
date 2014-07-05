edb95d0e4be0bb7a83fe05fe6285d85546c12a02
Ionică Bizău <bizauionica@yahoo.com>
2014-07-05 22:04:50 +0300

    Initial concept

---
+++ index.js
@@ -0,0 +1,59 @@
+// Dependencies
+var Request = require("request")
+  , Mandrill = require("mandrill-api/mandrill")
+  , Config = require("./config")
+  , MandrillClient = new Mandrill.Mandrill(Config.mandrillKey)
+  , Http = require("http")
+  , Url = require("url")
+  ;
+
+// The request url where the results are supposed to be posted
+const REQ_URL = Url.parse(
+    Config.reqUrl || "http://static.bacalaureat.edu.ro/2014/rapoarte/BH/index.html"
+);
+
+
+/**
+ * sendEmail
+ * Sends the notification email
+ *
+ * @name sendEmail
+ * @function
+ * @return
+ */
+function sendEmail() {
+    MandrillClient.messages.send({
+        message: {
+            from_email: Config.from.email
+          , from_name: Config.from.name
+          , to: [
+                {
+                    email: Config.to.email
+                  , name: Config.to.name
+                }
+            ]
+          , subject: Config.subject
+          , text: Config.text
+        }
+    }, function(result) {
+        console.log(result);
+    }, function (error) {
+        console.log(error);
+    });
+}
+
+function checkUrl() {
+    var options = {
+        method: 'HEAD'
+      , host: REQ_URL.host
+      , port: 80
+      , path: REQ_URL.path
+    }
+  , req = Http.request(options, function(res) {
+        console.log(JSON.stringify(res.statusCode));
+    });
+
+    req.end();
+}
+
+checkUrl();
