d9fa6a3f9c780805a33337d2a7cdd0782c97cf00
Ionică Bizău <bizauionica@yahoo.com>
2014-07-06 20:47:47 +0300

    Removed obsolete outputs

--- test/index.js
+++ test/index.js
@@ -7,19 +7,17 @@ var Statique = require("statique")
 var posted = false;
 setTimeout(function () {
     posted = true;
-}, 30 * 1000);
+}, 10 * 1000);
 
 // Statique config
 Statique.setRoutes({
     "/": function (req, res) {
         var msg =  "Hey you, nothing here yet.";
         if (!posted) {
-            console.log(msg);
             return Statique.error(req, res, 404, msg);
         }
         msg =  "Awesome results."
-            console.log(msg);
-        return Statique.error(req, res, 200, msg);
+        return Statique.sendRes(res, 200, "text", msg);
     }
 });
 
